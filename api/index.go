3b7c94884d1fb4fccccf709e734cc477850606c8
xiaozhou26 <xiaoxiaofeihh@foxmail.com>
2024-04-06 19:01:48 +0800

    12

--- api/index.go
+++ api/index.go
@@ -1,45 +1,25 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
-var router *gin.Engine
-
-var apiUrl = "https://api.telegram.org"
-
 func init() {
-	router = gin.Default()
-	router.Any("/*path", func(context *gin.Context) {
-		uri := context.Param("path")
-		if !strings.Contains(uri, "bot") {
-			context.String(http.StatusNotFound, "404 Not found")
-			return
-		}
-		url := apiUrl + uri
-		req, err := http.NewRequestWithContext(context, context.Request.Method, url, context.Request.Body)
-		if err != nil {
-			fmt.Println(err)
-			context.String(http.StatusBadRequest, err.Error())
-			return
-		}
-		req.Header = context.Request.Header
-		req.PostForm = context.Request.PostForm
-		req.Form = context.Request.Form
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			fmt.Println(err)
-			context.String(http.StatusBadRequest, err.Error())
-			return
-		}
-		context.DataFromReader(resp.StatusCode, resp.ContentLength, "application/json", resp.Body, nil)
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello, World!")
 	})
+
+	http.Handle("/", router)
 }
 
 func Listen(w http.ResponseWriter, r *http.Request) {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello, World!")
+	})
+
 	router.ServeHTTP(w, r)
 }
